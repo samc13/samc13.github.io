e360775f0dd6bb8f191a6c3987c09aa88b96889e
SamClarke <sam.c94@hotmail.com>
2025-08-14 22:01:32 +0100

    Okay, we're loading up the blogs and can render them on screen. Next we'll need to make this all look reasonable

--- site/src/app/home/Contents.tsx
+++ site/src/app/home/Contents.tsx
@@ -12,6 +12,7 @@ export default function Contents() {
         <Link href="/career" text="Career." newTab={false} />
         <Link href="/running/parkruns" text="Parkruns." newTab={false} />
         <Link href="/running/strava/2025" text="Strava." newTab={false} />
+        <Link href="/blog" text="Blog." newTab={false} />
       </div>
     </Fragment>
   );
