3436ba92765a2a3186c12d5a383c6c4ac485879a
SamClarke <sam.c94@hotmail.com>
2025-08-18 21:43:43 +0100

    A load of markdown styling

--- site/src/app/blog/BlogPost.tsx
+++ site/src/app/blog/BlogPost.tsx
@@ -1,10 +1,15 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import ReactMarkdown from "react-markdown";
 import clsx from "clsx";
+import markdown from "./../core/markdown.module.scss";
+import Markdown from "react-markdown";
+import Card from "../scaffolding/Card";
+import Content from "../scaffolding/Content";
+import { formatBlogPostDate } from "../utils/DateUtils";
 
-const BLOG_RAW_URL = "https://raw.githubusercontent.com/samc13/public-blog/refs/heads/main/blogs/";
+const BLOG_RAW_URL =
+  "https://raw.githubusercontent.com/samc13/public-blog/refs/heads/main/blogs/";
 
 export default function BlogPost({ filename }: { filename: string }) {
   const [content, setContent] = useState<string>("");
@@ -12,13 +17,18 @@ export default function BlogPost({ filename }: { filename: string }) {
   useEffect(() => {
     if (!filename) return;
     fetch(BLOG_RAW_URL + filename)
-      .then(res => res.text())
+      .then((res) => res.text())
       .then(setContent);
   }, [filename]);
 
-  return (
-    <div>
-      {filename ? <ReactMarkdown>{content}</ReactMarkdown> : <></>}
+  return filename ? (
+    <div className={clsx(markdown["markdown-content"])}>
+      <Content>
+        <h1>{formatBlogPostDate(filename)}</h1>
+        <Markdown>{content}</Markdown>
+      </Content>
     </div>
+  ) : (
+    <></>
   );
 }
